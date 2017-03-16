54ba1e0171a85517d6eb0300a674838b1d7db2cc
Sunny-Dong <dongxinyu_whu@126.com>
2017-03-16 15:23:06 +0800

    整理

--- 01.IO编程/json.py
+++ 01.IO编程/json.py
@@ -8,3 +8,5 @@ Created on Thu Mar 16 14:40:32 2017
 import json
 d=dict(name='Kawhi Leonard',age=25,height=2.01)
 json.dumps(d)
+a='{"age": 25, "name": "Kawhi Leonard", "height": 2.01}'
+json.loads(a)
